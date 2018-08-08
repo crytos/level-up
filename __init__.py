90fe4facf6479cd64fdcabaad1b1af44893570da
= <=>
2018-08-08 17:33:59 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from Level35.signup import Signup
---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import unittest
+# from ..signup import Signup
+
+
+class SignupTestCase(unittest.TestCase):
+    "Runs test for signup module"
+
+    # def test_if_signup_object_is_created(self):
+    #     signup = Signup()
+    #     self.assertIsInstance(signup, Signup)
+    
+    # def test_if_signup_object_is_created(self):
+    #     signup = Signup()
+    #     self.assertIsInstance(signup, Signup)
+
+
